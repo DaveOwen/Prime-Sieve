4893d1f526b7e42d15398521c5cf20e2476dc52f
Owen <David.Owen@landregistry.gov.uk>
2019-01-28 17:50:58 +0000

    Updated main.cpp

--- PrimeSieve/main.cpp
+++ PrimeSieve/main.cpp
@@ -1,27 +1,26 @@
 //Written by: David Owen
 
 #include <iostream>
-#include <Windows.h>
-#include <bitset>
 #include <math.h>
+#include <cstring>
+#include <ctime>
+#include <chrono>
+#include <ratio>
 
-#define RUNS 10000  // How many times the code will run. I use run each section of code a set number of times, divide by that number
-// to get the average. I had to to this because the program was running too fast for the GetTickCount() to get a speed.
-#define RANGE 100000 // What number of primes to search up to
+#define RUNS 1000				// How many times the code will run. This is used to obtain a more accurate timing.
+#define RANGE 100000 		// What number of primes to search up to
 #define R2 RANGE/2
-#define BRUNS 100000
 
-
+using namespace std::chrono;
 int main()
 {
 	//Set up varibles
 	int count;
 	char primes[RANGE];
 	int searchRange = sqrt(R2) + 1;
-	DWORD startTime, endTime;
 
 	//############################ SIEV #################################
-	startTime = GetTickCount();
+	high_resolution_clock::time_point startTime = high_resolution_clock::now();
 
 	for (int k = 0; k < RUNS; ++k)
 	{
@@ -39,28 +38,17 @@ int main()
 			}
 		}
 	}
-	endTime = GetTickCount();
+	high_resolution_clock::time_point endTime = high_resolution_clock::now();
 	for (int i = 0; i < R2; i++) if (primes[i] == 0) count++;
 
-	float totalOp = ((float)endTime - startTime) / (1000 * RUNS);
+	duration<double> time_span = duration_cast<duration<double>>((endTime - startTime) / (1000 * RUNS));
 	//#####################################################################
-
-
-
-	//#################### MEMSET FUNCTION TIMING TEST ####################
-	// Here I am timing how long the memset function takes, so I can take that off the time for the sieve
-
-	float totalMemset = ((float)endTime - startTime) / (1000 * RUNS);
-	//#####################################################################
-
 
 
 
 	//####################  OUTPUT TIME AND PRIMES  ######################
-	std::cout << "Optimised Sieve Time:  ";
-	std::cout << totalOp;
-	std::cout << "\nPrimes Found: ";
-	std::cout << count + 1;
+	std::cout << "Optimised Sieve Time:  " << time_span.count() << std::endl;
+	std::cout << "\nPrimes Found:  " << count + 1 << std::endl;
 	//#####################################################################
 	return 0;
 }
