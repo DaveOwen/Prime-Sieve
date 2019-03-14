1c8d53caf7aa7c2a8f9a908ab5e602bf53c4ab25
David Owen <david.owen@landregistry.gov.uk>
2019-03-14 13:38:41 +0000

    Add OpenMP functionality

--- PrimeSieve/main.cpp
+++ PrimeSieve/main.cpp
@@ -6,44 +6,49 @@
 #include <ctime>
 #include <chrono>
 #include <ratio>
+#include <omp.h>
 
 #define RUNS 1000				// How many times the code will run. This is used to obtain a more accurate timing.
-#define RANGE 100000 		// What number of primes to search up to
+#define RANGE 1000000		// What number of primes to search up to
 #define R2 RANGE/2
 
 using namespace std::chrono;
+
 int main()
 {
 	//Set up varibles
 	int count;
-	char primes[RANGE];
-	int searchRange = sqrt(R2) + 1;
+	char* primes = new char[RANGE];
+	unsigned long int searchRange = sqrt(R2) + 1;
+	omp_set_nested(true);
 
 	//############################ SIEV #################################
 	high_resolution_clock::time_point startTime = high_resolution_clock::now();
-
-	for (int k = 0; k < RUNS; ++k)
+	#pragma omp parallel num_threads(8)
 	{
-		count = 0;
-		memset(primes, 0, R2);
-		for (int i = 0; i < searchRange; ++i)
+		#pragma omp for
+		for (int k = 0; k < RUNS; ++k)
 		{
-			if (primes[i] == 0)
+			memset(primes, 0, R2);
+			for (int i = 0; i < searchRange; ++i)
 			{
-				for (int j = (i << 1)*i + (i << 2) + (i << 1) + 3; j < R2; j += i * 4 + 6)
+				if (primes[i] == 0)
 				{
-					primes[j] = 1;
-					primes[j + i * 2 + 3] = 1;
+					for (int j = (i << 1)*i + (i << 2) + (i << 1) + 3; j < R2; j += i * 4 + 6)
+					{
+						primes[j] = 1;
+						primes[j + i * 2 + 3] = 1;
+					}
 				}
 			}
 		}
 	}
+
 	high_resolution_clock::time_point endTime = high_resolution_clock::now();
 	for (int i = 0; i < R2; i++) if (primes[i] == 0) count++;
 
-	duration<double> time_span = duration_cast<duration<double>>((endTime - startTime) / (1000 * RUNS));
+	duration<double> time_span = duration_cast<duration<double>>((endTime - startTime) / RUNS);
 	//#####################################################################
-
 
 
 	//####################  OUTPUT TIME AND PRIMES  ######################
